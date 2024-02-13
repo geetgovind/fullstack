9c97ff99c1f73501c98020ddb5d947296aafa347
geetgovind <geetg.2902@gmail.com>
2024-02-13 10:46:02 +0530

    Refracted cards

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,27 +1,55 @@
-import React from "react";
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-
-}
+import React from 'react';
+import Card from './card';
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
+  const users: user[] = await res.json();
+
   return (
-    <>
-      <h1>Users</h1>
-      <ul>
+    <div className="container mx-auto py-4">
+      <h1 className="text-3xl font-bold mb-4">Users</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {users.map((user) => (
-          <li key={user.id}>
-            {user.id}. {user.name} has username : {user.username} and email : {user.email}
-          </li>
+          <Card key={user.id} user={user} />
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   );
 };
 
 export default UsersPage;
+
+
+
+
+
+
+
+
+// import React from "react";
+// interface User {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+
+// }
+
+// const UsersPage = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const users: User[] = await res.json();
+//   return (
+//     <>
+//       <h1>Users</h1>
+//       <ul>
+//         {users.map((user) => (
+//           <li key={user.id}>
+//             {user.id}. {user.name} has username : {user.username} and email : {user.email}
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default UsersPage;
