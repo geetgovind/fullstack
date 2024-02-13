4c7871ca25986c487813d3d272405020f49508f1
geetgovind <geetg.2902@gmail.com>
2024-02-13 17:06:22 +0530

    reversed card refactoring added button

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 
 export default function Home() {
-  return <main>Hello World!!  
-    <Link href="/users">go to user page</Link>
-    <ProductCard />
-  </main>;
+  return (
+    <main className="container ">
+      Next App on Vercel&nbsp;
+      <div className="container">
+        <Link
+          href="/users"
+          className="bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+        >
+          go to user page
+        </Link>
+      </div>
+      <ProductCard />
+    </main>
+  );
 }
